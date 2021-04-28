d29a9206c73e46500bafb9f0bbd474a3aeecbd99
disjfa <disjfa@disjfa.nl>
2021-04-28 22:05:23 +0200

    Updated screenshots

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const Encore = require('@symfony/webpack-encore');
 
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
   .setOutputPath('build/')
   .setPublicPath('/bootstrap-blocks/build')
   .setManifestKeyPrefix('build')
   .addEntry('app', './js/app.js')
+  .splitEntryChunks()
   .enableSingleRuntimeChunk()
   .enableSourceMaps(!Encore.isProduction())
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = 'usage';
+    config.corejs = 3;
+  })
   .enableSassLoader()
   .autoProvidejQuery()
 ;
