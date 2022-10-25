4900724f98affbd067fc5ac3b9ffc8f816f204a3
disjfa <disjfa@disjfa.nl>
2022-10-25 23:22:12 +0200

    Readded screenshot script

--- screenshot.js
+++ screenshot.js
@@ -1,39 +1,68 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const request = require('request');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 const baseUrl = 'http://localhost:8080/bootstrap-blocks';
 
-request.get({
-  url: baseUrl + '/api/pages.json',
-  json: true,
-  headers: { 'User-Agent': 'request' }
-}, (err, res, data) => {
-  if (err) {
-    console.error('Error:', err);
-  } else if (res.statusCode !== 200) {
-    console.error('Status:', res.statusCode);
-  } else {
-    for (let i of data) {
-      const folderName = 'screenshot' + i.base;
-      console.log(folderName);
-      fs.mkdir(folderName, { recursive: true }, (err) => {
-        if (err) throw err;
-      });
-
-      (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.setViewport({
-          width: 1280,
-          height: 960,
-          deviceScaleFactor: 1,
+fetch(baseUrl + '/api/pages.json')
+  .then(res => {
+    res.json().then(data => {
+      for (let i of data) {
+        const folderName = 'screenshot' + i.base;
+        console.log(folderName);
+        fs.mkdir(folderName, { recursive: true }, (err) => {
+          if (err) throw err;
         });
-        await page.goto('http://localhost:8080' + i.url);
-        await page.screenshot({ path: folderName + '/screenshot.png' });
 
-        await browser.close();
-      })();
-    }
-  }
-});
+        (async () => {
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+          await page.setViewport({
+            width: 1280,
+            height: 960,
+            deviceScaleFactor: 1,
+          });
+          await page.goto('http://localhost:8080' + i.url);
+          await page.screenshot({ path: folderName + '/screenshot.png' });
 
+          await browser.close();
+        })();
+      }
+
+    });
+  });
+
+// request.get({
+//   url: baseUrl + '/api/pages.json',
+//   json: true,
+//   headers: { 'User-Agent': 'request' }
+// }, (err, res, data) => {
+//   if (err) {
+//     console.error('Error:', err);
+//   } else if (res.statusCode !== 200) {
+//     console.error('Status:', res.statusCode);
+//   } else {
+//     for (let i of data) {
+//       const folderName = 'screenshot' + i.base;
+//       console.log(folderName);
+//       fs.mkdir(folderName, { recursive: true }, (err) => {
+//         if (err) throw err;
+//       });
+//
+//       (async () => {
+//         const browser = await puppeteer.launch();
+//         const page = await browser.newPage();
+//         await page.setViewport({
+//           width: 1280,
+//           height: 960,
+//           deviceScaleFactor: 1,
+//         });
+//         await page.goto('http://localhost:8080' + i.url);
+//         await page.screenshot({ path: folderName + '/screenshot.png' });
+//
+//         await browser.close();
+//       })();
+//     }
+//   }
+// });
+//
